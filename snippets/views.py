13531bbf14dc973b4c1db0e5bbf441c7ffcfb402
KellyKiiru <infowithkiiru@gmail.com>
2023-08-18 03:01:19 +0300

    comment why we use @api_view decorator

--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,5 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view # we use @api_view decorators for function based views
 from rest_framework.response import Response
 
 from .models import Snippet
