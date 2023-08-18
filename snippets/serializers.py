73827b13c9d64828a473f68ddccb5515275a11cd
KellyKiiru <infowithkiiru@gmail.com>
2023-08-18 05:45:20 +0300

    adding HyperlinkedModelSerializer  to enable hyperlinking

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,20 +1,21 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+
 from .models import *
 
-"""
-serializer.ModelSerializer makes it easier to serialize your models. You write less code with it
-"""
-class SnippetSerializer(serializers.ModelSerializer):
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source = 'owner.username')
+    highlight = serializers.HyperlinkedRelatedField(view_name='snippet-highlight', format='html')
+
     class Meta:
         model = Snippet
         # fields = ['__all__']
-        fields = ['id', 'owner', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['url', 'id', 'highlight', 'owner', 'title', 'code', 'linenos', 'language', 'style']
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ['id','username','snippets']
+        fields = ['url','id','username','snippets']
