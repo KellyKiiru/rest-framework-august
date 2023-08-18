4a6f8658a0a8c629f46687a24ab032016bd39575
KellyKiiru <infowithkiiru@gmail.com>
2023-08-18 04:36:26 +0300

    define UserSerializer

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,38 +1,20 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import *
-
-"""
-Using serializers.Serializers is encouraged for when you want to explicitly declare the fields you want to serialize
-"""
-
-# class SnippetSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-#     code = serializers.CharField(style={'base_template':'textarea.html'})
-#     linenos = serializers.BooleanField(required=False)
-#     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-#     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
-
-#     def create(self, validated_data):
-
-#         return Snippet.objects.create(**validated_data)
-    
-#     def update(self, instance, validated_data):
-#         instance.title = validated_data.get('title', instance.title)
-#         instance.code = validated_data.get('code', instance.code)
-#         instance.linenos = validated_data.get('linenos', instance.linenos)
-#         instance.language = validated_data.get('language', instance.language)
-#         instance.style = validated_data.get('style', instance.style)
-
-#         instance.save()
-
-#         return instance
 
 """
 serializer.ModelSerializer makes it easier to serialize your models. You write less code with it
 """
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source = 'owner.username')
     class Meta:
         model = Snippet
         # fields = ['__all__']
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'owner', 'title', 'code', 'linenos', 'language', 'style']
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id','username','snippets']
