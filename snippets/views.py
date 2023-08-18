29e8e0a4ff8644e4c26af54e074392dc294fb15b
KellyKiiru <infowithkiiru@gmail.com>
2023-08-18 05:21:56 +0300

    define fucntion based view for the api root

--- snippets/views.py
+++ snippets/views.py
@@ -1,14 +1,21 @@
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from rest_framework import generics 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from .permissions import IsOwnerOrReadOnly
 
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
 
-"""
-Using the mixin classes we've rewritten the views to use slightly less code than before, but we can go one step further. REST framework provides a set of already mixed-in generic views that we can use to trim down our views.py module even more.
-"""
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
 
 class SnippetList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
