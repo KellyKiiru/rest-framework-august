506c48eb0b342b031c26a165fbd604b330afb981
KellyKiiru <infowithkiiru@gmail.com>
2023-08-18 05:37:09 +0300

    define SnippetHighlight class based view

--- snippets/views.py
+++ snippets/views.py
@@ -4,8 +4,9 @@ from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-from .permissions import IsOwnerOrReadOnly
+from rest_framework import renderers
 
+from .permissions import IsOwnerOrReadOnly
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
 
@@ -39,3 +40,11 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
