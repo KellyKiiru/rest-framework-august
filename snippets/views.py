443a7daeb814f78ee11cbf559767e79bb084a58f
KellyKiiru <infowithkiiru@gmail.com>
2023-08-18 04:37:04 +0300

    define UserList and UserDetail class-based views

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import User
 from .models import Snippet
-from .serializers import SnippetSerializer
+from .serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics 
+
 """
 Using the mixin classes we've rewritten the views to use slightly less code than before, but we can go one step further. REST framework provides a set of already mixed-in generic views that we can use to trim down our views.py module even more.
 """
@@ -8,9 +10,23 @@ Using the mixin classes we've rewritten the views to use slightly less code than
 
 
 class SnippetList(generics.ListCreateAPIView):
+    """
+    save user instance when snippet is created
+    """
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):  
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
